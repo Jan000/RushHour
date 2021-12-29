d286bb911465474503f2593326a21a97dd2f657d
Jan <contact@jandev.net>
2021-12-29 23:01:08 +0100

    License added to all sources

--- src/de/matthiasmann/twl/utils/PNGDecoder.java
+++ src/de/matthiasmann/twl/utils/PNGDecoder.java
@@ -29,7 +29,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.jkdev.rushhour.util;
+package de.matthiasmann.twl.utils;
 
 import java.io.EOFException;
 import java.io.IOException;
